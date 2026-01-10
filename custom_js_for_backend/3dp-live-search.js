b58c805d5117ebd1c46b312a92bcc92a7162bb91
rakesh-shishodia <shishodia.rakesh@gmail.com>
2026-01-10 14:18:35 +0530

    Added custom js file for 3dp live search functionality. This file will be hosted in app backend by ecwid app support

---
+++ custom_js_for_backend/3dp-live-search.js
@@ -0,0 +1,8 @@
+<script>
+  (function () {
+    var s = document.createElement('script');
+    s.src = 'https://rakesh-shishodia.github.io/ecwid-live-search/frontend/search.js';
+    s.defer = true;
+    document.head.appendChild(s);
+  })();
+</script>
